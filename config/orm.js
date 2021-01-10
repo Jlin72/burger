35111a1af8f7518fac0ee62842b5945dba32f7fd
Jhonny Lin <jhonnylin72@gmail.com>
2021-01-10 14:36:48 -0500

    Wrote the three functions needed for the app
    These three functions will call upon the mysql database and either get the results or modify it

--- config/orm.js
+++ config/orm.js
@@ -2,13 +2,43 @@ const { table } = require('console');
 const connection = require('./connection');
 
 const orm = {
-    selectTable(tablename) {
+    //SelectAll will select all the data from one table
+    selectAll(tablename) {
         connection.query(
             `SELECT * FROM ??`,
             [tablename],
             (err, res) => {
                 if(err) throw err;
                 console.log(res);
+            }
+        );
+    },
+    //Insert into a table
+    insertOne(tablename, column, value) {
+        connection.query(
+            `INSERT INTO ?? (??) VALUES (?)`,
+            [tablename, column, value],
+            (err, result) => {
+                if(err) res.status(500).end();
+                console.log({id: result.insertId});
+                res.json ({id: result.insertId});
+            }
+        )
+    },
+
+    //Update a table
+    updateOne(tablename, column, boolean, id, idNumber) {
+        connection.query(
+            `UPDATE ?? SET ?? = ? WHERE ?? = ?`,
+            [tablename, column, boolean, id, idNumber],
+            (err, result) => {
+                if(err) {
+                    res.status(500).end();
+                }
+                if(result.changedRows === 0) {
+                    res.status(400).end();
+                }
+                res.status(200).end();
             }
         );
     }
